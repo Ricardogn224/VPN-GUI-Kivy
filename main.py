56588674556219b5157b78b0f9eba8258db0851b
Ricardo <abrahamricardogn224@gmail.com>
2023-11-27 15:10:16 +0100

    app update

--- main.py
+++ main.py
@@ -1,45 +1,90 @@
+import subprocess
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-import pyopenvpn
+from kivy.uix.checkbox import CheckBox
 
 
 class VPNApp(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
 
-        self.username_input = TextInput(hint_text='Username')
-        self.password_input = TextInput(hint_text='Password', password=True)
-        self.connect_button = Button(
-            text='Connect VPN', on_press=self.connect_vpn)
+        # Add the existing widgets
+        self.vpn_button = Button(text='Connect VPN', on_press=self.connect_vpn)
+        self.status_label = Label(text='VPN Status: Not Connected')
+        self.layout.add_widget(self.vpn_button)
+        self.layout.add_widget(self.status_label)
 
-        self.layout.add_widget(self.username_input)
-        self.layout.add_widget(self.password_input)
-        self.layout.add_widget(self.connect_button)
+        # Add the new widgets
+        self.login_page = BoxLayout(orientation='vertical')
+
+        self.username_label = Label(text='Username:')
+        self.username_input = TextInput(text='')
+
+        self.password_label = Label(text='Password:')
+        self.password_input = TextInput(text='', password=True)
+
+        self.remember_me_checkbox = CheckBox()  # Remove the 'text' keyword argument
+
+        self.login_page.add_widget(self.username_label)
+        self.login_page.add_widget(self.username_input)
+        self.login_page.add_widget(self.password_label)
+        self.login_page.add_widget(self.password_input)
+        self.login_page.add_widget(self.remember_me_checkbox)
+
+        self.layout.add_widget(self.login_page)
 
         return self.layout
 
     def connect_vpn(self, instance):
+        process = subprocess.Popen(['ls', '-l'], stdout=subprocess.PIPE)
+        output, err = process.communicate()
+        print(output.decode('utf-8'))
 
-        # Récupérer les informations de l'utilisateur
+        # Define the OpenVPN configuration file path
+        config_file_path = '/path/to/config/file.ovpn'
+
+        # Get the username and password
         username = self.username_input.text
         password = self.password_input.text
 
-        # Paramètres du serveur OpenVPN
-        host = 'your_openvpn_server_host'
-        port = 443  # Le port peut varier en fonction de la configuration
+        # Check if the user wants to remember the credentials
+        remember_me = self.remember_me_checkbox.active
 
-        # Création d'un client OpenVPN
+        # Start the OpenVPN client in a background process
+        if remember_me:
+            # Save the credentials
+            self.save_credentials(username, password)
 
-        vpn = pyopenvpn.Openvpn(host, port)
+            process = subprocess.Popen(['openvpn', config_file_path, '--username',
+                                        username, '--password', password], stdout=PIPE, stderr=PIPE)
+        else:
+            process = subprocess.Popen(['openvpn', config_file_path],
+                                       stdout=PIPE, stderr=PIPE)
 
-        # Connexion au serveur OpenVPN
-        try:
-            vpn.connect(username, password)
-            print("Connexion VPN établie !")
-        except Exception as e:
-            print("Erreur lors de la connexion au VPN :", e)
+        # Capture the output of the OpenVPN client
+        stdout, stderr = process.communicate()
+
+        # Check if the VPN connection was successful
+        if 'Initialization Sequence Completed' in stdout:
+            self.status_label.text = 'VPN Status: Connected'
+        else:
+            self.status_label.text = f'VPN Status: Error - {stderr}'
+
+    def save_credentials(self, username, password):
+        # Open the preferences file
+        preferences = open('preferences.ini', 'w')
+
+        # Write the username
+        preferences.write('username = {}\n'.format(username))
+
+        # Write the password
+        preferences.write('password = {}\n'.format(password))
+
+        # Close the preferences file
+        preferences.close()
 
 
 if __name__ == '__main__':
